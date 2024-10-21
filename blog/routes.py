151f1373310ed1ca32be53e9213be0f09adb14cb
Jan Kuncewicz <jankuncewicz@gmail.com>
2024-10-22 01:02:23 +0200

    rebuild of refactor

--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,18 @@
-# blog/routes.py
 
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
+from blog.forms import LoginForm
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
             
 @app.route("/")
 def index():
@@ -15,43 +24,57 @@ def create_edit_entry(entry_id=None):
     errors = None
     if entry_id is not None: # edit
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
-        form = EntryForm(obj=entry) 
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            return form
-        else:
-            errors = form.errors
-            return errors
+        form = EntryForm(obj=entry)
     else:
-        form = EntryForm() # create
-        if form.validate_on_submit():
-            entry = Entry(
+        form = EntryForm()
+        entry = Entry(
                 title=form.title.data,
                 body=form.body.data,
                 is_published=form.is_published.data
-            )
-            db.session.add(entry)
+        )
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if entry_id is not None:
+                form.populate_obj(entry)
+            else:
+                db.session.add(entry)
             db.session.commit()
-            return form
+            return redirect(url_for('index'))
         else:
             errors = form.errors
-            return errors
-            
+    return render_template("entry_form.html", form=form, errors=errors)            
     
 
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
 def create_entry():
-    if request.method == 'POST':
-        form = create_edit_entry()
-    return render_template("entry_form.html", form=form, errors=form)
+    return create_edit_entry()
  
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
-    if request.method == 'POST': 
-        form = create_edit_entry(entry_id)
-    return render_template("entry_form.html", form=form, errors=form)
+    return create_edit_entry(entry_id)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
 
 
-    
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
 
--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))    # 1
@@ -10,3 +9,6 @@ class Config:
            'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
+        
